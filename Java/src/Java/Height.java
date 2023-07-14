4d25c8f4a6d568d156aa6db85916d3208fea50e5
zcliu <2103010312@hhu.edu.cn>
2023-07-14 18:42:58 +0800

    3

---
+++ Java/src/Java/Height.java
@@ -0,0 +1,21 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Height 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		int FatherHeight = 0;
+		int MotherHeight = 0;
+		double SonHeight = 0;
+		System.out.println("please input your father's height:"+"cm");
+		FatherHeight = in.nextInt();
+		System.out.println("please input your mother's height:"+"cm");
+		MotherHeight = in.nextInt();
+		System.out.println("son's height is "+(FatherHeight+MotherHeight)*1.08/2+"cm");
+	}
+
+}
