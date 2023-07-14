318f2237b3b381d904e9a5d59f9573e2f6a9bffd
zcliu <2103010312@hhu.edu.cn>
2023-07-14 18:31:31 +0800

    2

---
+++ Java/src/Java/Num.java
@@ -0,0 +1,17 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Num 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		int num_1 = 1;
+		int num_2 = 2;
+		System.out.println("Sum = "+(num_1+num_2));
+		in.close();
+	}
+
+}
