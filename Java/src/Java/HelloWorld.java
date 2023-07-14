2c208396d761c45001ebcdb41b2d7413a15a1888
zcliu <2103010312@hhu.edu.cn>
2023-07-14 18:21:01 +0800

    1

---
+++ Java/src/Java/HelloWorld.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class HelloWorld 
+
+{
+	
+	public static void main(String[] args) 
+	
+	{
+		
+		System.out.println("Hello World!");
+		
+		System.out.println("I will be a successful Java engineering!");
+		
+	}
+	
+}
