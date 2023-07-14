5e43017bb1ccb722fa82fd765cc0d273123655f5
ZCliu2002 <2103010312@hhu.edu.cn>
2023-07-14 19:02:22 +0800

    hello

---
+++ Java/src/Java/Judgement.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class Judgement 
+{
+
+	public static void main(String[] args) 
+	{
+		int red = 25;
+		int blue = 30;
+		red = red*2+10;
+		blue = blue*2;
+		System.out.println(red == blue);
+	}
+
+}
